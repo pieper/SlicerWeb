3b4ef29f004466095e9bded2d1820e1f6031b36e
Steve Pieper <pieper@bwh.harvard.edu>
2012-05-25 16:57:10 -0400

    ENH: switch to BaseHTTPServer (no more cherrypy)

--- WebServer/Helper/ServerHelper.py
+++ WebServer/Helper/ServerHelper.py
@@ -1,211 +1,230 @@
-import sys,os
+import string,cgi,time
+import os,sys
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
-class ServerHelper(object):
+class SlicerRequestHandler(BaseHTTPRequestHandler):
+
+  def start_response(self, status, response_headers):
+    self.send_response(status)
+    for keyword,value in response_headers:
+      self.send_header(keyword, value)
+    self.end_headers()
+
+  def logMessage(self, message):
+     self.server.logMessage(message)
+
+  def do_GET(self):
+    try:
+      status = 200
+      rest = self.path
+
+      if os.path.dirname(rest).endswith('slicer'):
+        self.logMessage('splitting path')
+        rest = '/slicer/' + os.path.split(rest)[1]
+      self.logMessage("Handling: " + rest)
+
+      # talk with slicer
+      if rest.find("/slicer") == 0:
+        if self.server.communicatingWithSlicer:
+          response_headers = [('Content-Type','text/plain')]
+          self.start_response(status, response_headers)
+          self.wfile.write( 'Busy' )
+        else:
+          self.server.communicatingWithSlicer = True
+          subcmd = rest[len("/slicer"):]
+          sys.stdout.write(subcmd + "\n")
+          sys.stdout.flush()
+          if subcmd.find("/repl") == 0:
+            count = int(sys.stdin.readline())
+            im = sys.stdin.read(count)
+            self.server.communicatingWithSlicer = False
+            response_headers = [('Content-Type','text/plain')]
+            self.start_response(status, response_headers)
+            self.wfile.write( im )
+          elif subcmd.find("/mrml") == 0:
+            count = int(sys.stdin.readline())
+            im = sys.stdin.read(count)
+            self.server.communicatingWithSlicer = False
+            response_headers = [('Content-Type','application/json')]
+            self.start_response(status, response_headers)
+            self.wfile.write( im )
+          elif subcmd.find("/scene") == 0:
+            count = int(sys.stdin.readline())
+            im = sys.stdin.read(count)
+            self.server.communicatingWithSlicer = False
+            response_headers = [('Content-Type','application/json')]
+            self.start_response(status, response_headers)
+            self.wfile.write( im )
+          elif subcmd.find("/timeimage") == 0:
+            count = int(sys.stdin.readline())
+            im = sys.stdin.read(count)
+            self.server.communicatingWithSlicer = False
+            response_headers = [('Content-Type','image/png')]
+            self.start_response(status, response_headers)
+            self.wfile.write( im )
+          elif subcmd.find("/slice") == 0:
+            count = int(sys.stdin.readline())
+            self.logMessage('trying to read %d from stdin' % count)
+            im = sys.stdin.read(count)
+            self.server.communicatingWithSlicer = False
+            response_headers = [('Content-Type','image/png')]
+            self.start_response(status, response_headers)
+            self.wfile.write( im )
+          elif subcmd.find("/threeD") == 0:
+            count = int(sys.stdin.readline())
+            im = sys.stdin.read(count)
+            self.server.communicatingWithSlicer = False
+            response_headers = [('Content-Type','image/png')]
+            self.start_response(status, response_headers)
+            self.wfile.write( im )
+          elif subcmd.find("/transform") == 0:
+            count = int(sys.stdin.readline())
+            im = sys.stdin.read(count)
+            self.server.communicatingWithSlicer = False
+            response_headers = [('Content-Type','image/png')]
+            self.start_response(status, response_headers)
+            self.wfile.write( im )
+          elif subcmd.find("/volumeSelection") == 0:
+            count = int(sys.stdin.readline())
+            im = sys.stdin.read(count)
+            self.server.communicatingWithSlicer = False
+            response_headers = [('Content-Type','image/png')]
+            self.start_response(status, response_headers)
+            self.wfile.write( im )
+          elif subcmd.find("/volume") == 0:
+            count = int(sys.stdin.readline())
+            im = sys.stdin.read(count)
+            self.logMessage("read %d from slicer" % count)
+            self.server.communicatingWithSlicer = False
+            response_headers = [('Content-Type','application/octet-stream')]
+            self.start_response(status, response_headers)
+            self.wfile.write( im )
+          elif subcmd.endswith("png"):
+            count = int(sys.stdin.readline())
+            im = sys.stdin.read(count)
+            self.server.communicatingWithSlicer = False
+            response_headers = [('Content-Type','image/png')]
+            self.start_response(status, response_headers)
+            self.wfile.write( im )
+          else:
+            # didn't match known slicer API commands, so we shouldn't
+            # prevent other slicer connections from completing
+            count = int(sys.stdin.readline())
+            slicerResponse = sys.stdin.read(count)
+            self.server.communicatingWithSlicer = False
+            response_headers = [('Content-Type','text/plain')]
+            self.start_response(status, response_headers)
+            self.wfile.write( slicerResponse )
+
+      else:
+        # handle regular doctypes
+        if rest.endswith(".html"):
+          response_headers = [('Content-Type','text/html')]
+          self.start_response(status, response_headers)
+          html_path = self.server.docroot + rest
+          fp = open(html_path, 'r')
+          resp = fp.read()
+          fp.close()
+          self.wfile.write( resp )
+        elif rest.endswith(".json"):
+          response_headers = [('Content-Type','application/json')]
+          self.start_response(status, response_headers)
+          html_path = self.server.docroot + rest
+          fp = open(html_path, 'r')
+          resp = fp.read()
+          fp.close()
+          self.wfile.write( resp )
+        elif rest.endswith(".js"):
+          response_headers = [('Content-Type','application/javascript')]
+          self.start_response(status, response_headers)
+          html_path = self.server.docroot + rest
+          fp = open(html_path, 'r')
+          resp = fp.read()
+          fp.close()
+          self.wfile.write( resp )
+        elif rest.endswith(".css"):
+          response_headers = [('Content-Type','text/css')]
+          self.start_response(status, response_headers)
+          html_path = self.server.docroot + rest
+          fp = open(html_path, 'r')
+          resp = fp.read()
+          fp.close()
+          self.wfile.write( resp )
+        elif rest.endswith("png"):
+          response_headers = [('Content-Type','image/png')]
+          self.start_response(status, response_headers)
+          html_path = self.server.docroot + rest
+          fp = open(html_path, 'r')
+          resp = fp.read()
+          fp.close()
+          self.wfile.write( resp )
+        elif rest.endswith("ico"):
+          response_headers = [('Content-Type','image/vnd.microsoft.icon')]
+          self.start_response(status, response_headers)
+          html_path = self.server.docroot + rest
+          fp = open(html_path, 'r')
+          resp = fp.read()
+          fp.close()
+          self.wfile.write( resp )
+
+    except IOError:
+      self.send_error(404,'File Not Found: %s' % self.path)
+
+    self.logMessage("handled %s" % self.path)
+
+  def do_PUT(self):
+    try:
+      ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+      if ctype == 'multipart/form-data':
+        query=cgi.parse_multipart(self.rfile, pdict)
+        self.send_response(301)
+        self.end_headers()
+        upfilecontent = query.get('upfile')
+        # print "filecontent", upfilecontent[0]
+        self.wfile.write("<HTML>PUT OK.<BR><BR>");
+        self.wfile.write(upfilecontent[0]);
+        xx = """
+            if environ['REQUEST_METHOD'] == 'PUT':
+              #TODO
+              self.logMessage("got a PUT")
+              body = cherrypy.request.body.read()
+              self.logMessage("body is: %s" % body)
+        """
+    except :
+        pass
+
+    def do_POST(self):
+        #TODO
+        pass
+
+
+class SlicerHTTPServer(HTTPServer):
   """ This runs in a process that listens for web
   requests and then communicates with slicer as a parent
   program by communicating via stdio"""
   # TODO: set header so client knows that image refreshes are needed (avoid
   # using the &time=xxx trick)
-  def __init__(self, docroot='.', logFile=None, port=8070, server_name='SlicerWebServer'):
-    self.count = 0
-    self.port = port
-    self.server_name = server_name
+  def __init__(self, server_address=("",8070), RequestHandlerClass=SlicerRequestHandler, docroot='.', logFile=None):
+    HTTPServer.__init__(self,server_address, RequestHandlerClass)
     self.docroot = docroot
     self.logFile = logFile
     # use this as a mutex on the stdio connection
     self.communicatingWithSlicer = False
-
-  def app(self, environ, start_response):
-    """This is the actual app server that responds to web
-    requests by creating cherrypy ResponseObject instances.
-    http://www.cherrypy.org/wiki/ResponseObject
-    """
-    status = '200 OK'
-    rest = environ['PATH_INFO'] 
-    if os.path.dirname(rest).endswith('slicer'):
-      self.logMessage('splitting path')
-      rest = '/slicer/' + os.path.split(rest)[1]
-    self.logMessage("path is: " + rest)
-    if environ['QUERY_STRING'] != '':
-      rest += "?" + environ['QUERY_STRING']
-    self.logMessage("Handling: " + rest)
-    if rest.find("/env") == 0:
-      response_headers = [('Content-Type','text/plain')]
-      start_response(status, response_headers)
-      return [str(environ)]
-    if rest.find("/slicer") == 0:
-      if self.communicatingWithSlicer:
-        response_headers = [('Content-Type','text/plain')]
-        start_response(status, response_headers)
-        return ['Busy']
-      self.communicatingWithSlicer = True
-      subcmd = rest[len("/slicer"):]
-      sys.stdout.write(subcmd + "\n")
-      sys.stdout.flush()
-      if subcmd.find("/repl") == 0:
-        count = int(sys.stdin.readline())
-        im = sys.stdin.read(count)
-        self.communicatingWithSlicer = False
-        response_headers = [('Content-Type','text/plain')]
-        start_response(status, response_headers)
-        self.count += 1
-        return [im]
-      if subcmd.find("/mrml") == 0:
-        count = int(sys.stdin.readline())
-        im = sys.stdin.read(count)
-        self.communicatingWithSlicer = False
-        response_headers = [('Content-Type','application/json')]
-        start_response(status, response_headers)
-        self.count += 1
-        return [im]
-      if subcmd.find("/scene") == 0:
-        count = int(sys.stdin.readline())
-        im = sys.stdin.read(count)
-        self.communicatingWithSlicer = False
-        response_headers = [('Content-Type','application/json')]
-        start_response(status, response_headers)
-        self.count += 1
-        return [im]
-      if subcmd.find("/timeimage") == 0:
-        count = int(sys.stdin.readline())
-        im = sys.stdin.read(count)
-        self.communicatingWithSlicer = False
-        response_headers = [('Content-Type','image/png')]
-        start_response(status, response_headers)
-        self.count += 1
-        return [im]
-      if subcmd.find("/slice") == 0:
-        count = int(sys.stdin.readline())
-        im = sys.stdin.read(count)
-        self.communicatingWithSlicer = False
-        response_headers = [('Content-Type','image/png')]
-        start_response(status, response_headers)
-        self.count += 1
-        return [im]
-      if subcmd.find("/threeD") == 0:
-        count = int(sys.stdin.readline())
-        im = sys.stdin.read(count)
-        self.communicatingWithSlicer = False
-        response_headers = [('Content-Type','image/png')]
-        start_response(status, response_headers)
-        self.count += 1
-        return [im]
-      if subcmd.find("/transform") == 0:
-        count = int(sys.stdin.readline())
-        im = sys.stdin.read(count)
-        self.communicatingWithSlicer = False
-        response_headers = [('Content-Type','image/png')]
-        start_response(status, response_headers)
-        self.count += 1
-        return [im]
-      if subcmd.find("/volumeSelection") == 0:
-        count = int(sys.stdin.readline())
-        im = sys.stdin.read(count)
-        self.communicatingWithSlicer = False
-        response_headers = [('Content-Type','image/png')]
-        start_response(status, response_headers)
-        self.count += 1
-        return [im]
-      if subcmd.find("/volume") == 0:
-        count = int(sys.stdin.readline())
-        im = sys.stdin.read(count)
-        self.logMessage("read %d from slicer" % count)
-        self.communicatingWithSlicer = False
-        response_headers = [('Content-Type','application/octet-stream')]
-        start_response(status, response_headers)
-        self.count += 1
-        return [im]
-      if subcmd.endswith("png"):
-        count = int(sys.stdin.readline())
-        im = sys.stdin.read(count)
-        self.communicatingWithSlicer = False
-        response_headers = [('Content-Type','image/png')]
-        start_response(status, response_headers)
-        self.count += 1
-        return [im]
-      # didn't match known slicer API commands, so we shouldn't
-      # prevent other slicer connections from completing
-      count = int(sys.stdin.readline())
-      slicerResponse = sys.stdin.read(count)
-      self.communicatingWithSlicer = False
-      response_headers = [('Content-Type','text/plain')]
-      start_response(status, response_headers)
-      self.count += 1
-      return [slicerResponse]
-    # handle regular doctypes
-    if rest.endswith(".html"):
-      response_headers = [('Content-Type','text/html')]
-      start_response(status, response_headers)
-      html_path = self.docroot + rest
-      fp = open(html_path, 'r')
-      resp = fp.read()
-      fp.close()
-      return [resp]
-    if rest.endswith(".json"):
-      response_headers = [('Content-Type','application/json')]
-      start_response(status, response_headers)
-      html_path = self.docroot + rest
-      fp = open(html_path, 'r')
-      resp = fp.read()
-      fp.close()
-      return [resp]
-    if rest.endswith(".js"):
-      response_headers = [('Content-Type','application/javascript')]
-      start_response(status, response_headers)
-      html_path = self.docroot + rest
-      fp = open(html_path, 'r')
-      resp = fp.read()
-      fp.close()
-      return [resp]
-    if rest.endswith(".css"):
-      response_headers = [('Content-Type','text/css')]
-      start_response(status, response_headers)
-      html_path = self.docroot + rest
-      fp = open(html_path, 'r')
-      resp = fp.read()
-      fp.close()
-      return [resp]
-    if rest.endswith("png"):
-      response_headers = [('Content-Type','image/png')]
-      start_response(status, response_headers)
-      html_path = self.docroot + rest
-      fp = open(html_path, 'r')
-      resp = fp.read()
-      fp.close()
-      return [resp]
-    if rest.endswith("ico"):
-      response_headers = [('Content-Type','image/vnd.microsoft.icon')]
-      start_response(status, response_headers)
-      html_path = self.docroot + rest
-      fp = open(html_path, 'r')
-      resp = fp.read()
-      fp.close()
-      return [resp]
-    # don't know - send an error mesage
-    status = 404
-    response_headers = [('Content-Type','text/plain')]
-    start_response(status, response_headers)
-    self.count += 1
-    return [str(self.count)+" Didn't understand the message: " + rest]
 
   def start(self):
     """start the server
     - use one thread since we are going to communicate 
     via stdin/stdout, which will get corrupted with more threads
     """
-    global server
-    from cherrypy import wsgiserver
-    self.server = wsgiserver.CherryPyWSGIServer(
-        ('0.0.0.0', self.port), 
-        self.app,
-        numthreads=10,
-        server_name=self.server_name)
-
-    self.server.nodelay=False
-    self.server.start()
+    try:
+      self.logMessage('started httpserver...')
+      self.serve_forever()
+    except KeyboardInterrupt:
+      self.logMessage('KeyboardInterrupt - stopping')
+      self.stop()
 
   def stop(self):
-    self.server.stop()
+    self.socket.close()
 
   def logMessage(self,message):
     if self.logFile:
@@ -213,17 +232,11 @@ class ServerHelper(object):
       fp.write(message + '\n')
       fp.close()
 
-# When ServerHelper is run as a command line script, use
-# this to start the server itself
 if __name__ == '__main__':
-  global serverHelper
   if len(sys.argv) > 1:
     docroot = sys.argv[1]
   logFile = "/tmp/helper.log"
   if len(sys.argv) > 2:
     logFile = sys.argv[2]
-  serverHelper = ServerHelper(docroot=docroot, logFile=logFile, port=8070)
-  try:
-    serverHelper.start()
-  except KeyboardInterrupt:
-    serverHelper.stop()
+  server = SlicerHTTPServer(docroot=docroot,logFile=logFile)
+  server.start()
