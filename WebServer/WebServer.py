5c224fef28da120dc888beb3c1d40868c69a5867
Steve Pieper <pieper@isomics.com>
2012-06-12 14:16:32 -0400

    ENH: Fit the slices when loading the preset
    This makes the views look better in the remote app.

--- WebServer/WebServer.py
+++ WebServer/WebServer.py
@@ -308,6 +308,8 @@ class WebServerLogic:
       tumor2Display.SetAutoWindowLevel(0)
       tumor2Display.SetWindow(tumor1Display.GetWindow())
       tumor2Display.SetLevel(tumor1Display.GetLevel())
+      applicationLogic = slicer.app.applicationLogic()
+      applicationLogic.FitSliceToAll()
       return ( json.dumps([tumor1.GetName(), tumor2.GetName()]) )
     elif id == 'default':
       #
