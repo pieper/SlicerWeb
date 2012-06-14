00ca9bc9c138552998b0e3d3351423450baecbbc
Steve Pieper <pieper@isomics.com>
2012-06-14 08:54:41 -0400

    ENH: fix transform event stream (compress events)
    Ignore new events until previous ajax call completes.

--- WebServer/docroot/slicerjs/touchTransform.js
+++ WebServer/docroot/slicerjs/touchTransform.js
@@ -22,6 +22,16 @@ var touchTransform = function(options) {
             self.canvas.addEventListener('touchstart', self.onTouchStart, false);
             self.canvas.addEventListener('touchmove', self.onTouchMove, false);
             self.canvas.addEventListener('touchend', self.onTouchEnd, false);
+
+            self.waiting = false;
+        },
+
+        viewID: function() {
+            selection = $('#sliceModeBar').data('selected');
+            // TODO: decouple orientation from viewer
+            if (selection == 'Axial') { return 'Red'; }
+            if (selection == 'Sagittal') { return 'Yellow'; }
+            if (selection == 'Coronal') { return 'Green'; }
         },
 
         //
@@ -33,7 +43,7 @@ var touchTransform = function(options) {
             } else {
               // multitouch (only look at first 2 touch points)
               self.startX = (event.touches[0].pageX + event.touches[1].pageX)/2.;
-              self.startX = (event.touches[0].pageY + event.touches[1].pageY)/2.;
+              self.startY = (event.touches[0].pageY + event.touches[1].pageY)/2.;
               dx = event.touches[0].pageX - event.touches[1].pageX;
               dy = event.touches[0].pageY - event.touches[1].pageY;
               self.startDist = Math.sqrt( dx*dx + dy*dy );
@@ -47,26 +57,39 @@ var touchTransform = function(options) {
               // single touch
             } else {
               // multitouch (only look at first 2 touch points)
-              nowX = (event.touches[0].pageX + event.touches[1].pageX)/2.;
-              nowY = (event.touches[0].pageY + event.touches[1].pageY)/2.;
-              pan = {x: (nowX - self.startX), y: (nowY - self.startY)};
+              if (self.waiting) {
+                //self.log("skipped (waiting)");
+              } else {
+                nowX = (event.touches[0].pageX + event.touches[1].pageX)/2.;
+                nowY = (event.touches[0].pageY + event.touches[1].pageY)/2.;
+                pan = {x: (nowX - self.startX), y: (nowY - self.startY)};
 
-              dx = event.touches[0].pageX - event.touches[1].pageX;
-              dy = event.touches[0].pageY - event.touches[1].pageY;
-              nowDist = Math.sqrt( dx*dx + dy*dy );
+                dx = event.touches[0].pageX - event.touches[1].pageX;
+                dy = event.touches[0].pageY - event.touches[1].pageY;
+                nowDist = Math.sqrt( dx*dx + dy*dy );
 
-              self.zoom = self.startZoom * nowDist / self.startDist;
+                self.zoom = self.startZoom * nowDist / self.startDist;
 
-              zoomCenter = {x: nowX, y: nowY};
-              panZoom = {pan: pan, zoom: self.zoom, zoomCenter: zoomCenter};
-              self.setPanZoom(panZoom);
-              self.render();
+                zoomCenter = {x: nowX, y: nowY};
+                panZoom = {pan: pan, zoom: self.zoom, zoomCenter: zoomCenter};
+
+                url = 'slicer/transformInSlice?id=Transform-head';
+                url += '&view=' + self.viewID();
+                url += '&dx=' + (self.startX - nowX) / self.ctxt.canvas.width;
+                url += '&dy=' + (self.startY - nowY) / self.ctxt.canvas.height;
+                url += '&dl=0&theta=0';
+                self.waiting = true;
+                $.get(url, function(data) { 
+                    self.waiting = false;
+                });
+              }
             }
 
             event.preventDefault();
         },
 
         onTouchEnd: function(event) {
+            // TODO: need to track previous state - touch len is 0 here
             if (event.touches.length == 1) {
               // single touch
             } else {
@@ -94,7 +117,8 @@ $(function(){
     containerID: "touchTransformContainer",
   });
 
-  $.get('slicer/preset?id=transform', function(data){
+  $.get('slicer/preset?id=transform', "", function(data){
     // TODO: have a wait animation that gets cleared here...
+    // TODO: get the transform ID (currently hardcoded)
   });
 });
