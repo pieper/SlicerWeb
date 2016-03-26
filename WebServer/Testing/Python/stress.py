df95eada81e1568554808e39ee4776b0cb851c0b
Steve Pieper <pieper@isomics.com>
2016-03-26 17:48:45 -0400

    Improved stress test

--- WebServer/Testing/Python/stress.py
+++ WebServer/Testing/Python/stress.py
@@ -1,8 +1,14 @@
 
 import urllib
+import time
 
 url = 'http://localhost:2016/slicer/threeD'
+url = 'http://quantome.org:2016/slicer/threeD'
 filePath = '/tmp/threeD.png'
+interval = 0
 
 for req in range(100):
+  startTime = time.time()
   urllib.urlretrieve(url, filePath)
+  print "okay %d in %f" % (req, time.time() - startTime)
+  time.sleep(interval)
